8fcd75bd085a7a5d9c1b30f5dd41cc0c9c8fb466
Kelly Willard <kellywillard@Kellys-MacBook-Pro.local>
2019-11-18 17:24:53 -0800

    Refactor code to follow the seperation of concerns principle

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 document.body.onload = game;
 
-function game () { 
+function game () {
 
+var player1 = prompt("Player 1's Name", "Player 1");
+var player2 = prompt("Player 2's Name", "Player 2");
 
 
 var gameState = {
     currentPlayer: {X: true},
     model: null,
-    round: 0
+    round: 0,
+    player1: player1,
+    player2: player2
 }
 
 function findPlayer() {
     var player;
     if (gameState.currentPlayer.X) {
-        player = 'X'
+        player = gameState.player1
     } else {
-        player = 'O'
+        player = gameState.player2
     }
     return player;
 }
@@ -27,98 +31,107 @@ var playerEl = document.getElementById("current-player");
 playerEl.innerHTML= `Next Up: ${player}`;
 
 
-//create div box that will be divided into 9 sections
-var leaderboard = document.getElementById("leaderboard");
-leaderboard.setAttribute("style", "display: inline-grid; grid-template-columns: auto auto; text-align:center");
-
-
-
-var grid = document.getElementById("grid");
-grid.setAttribute("style", "display: inline-grid; grid-template-columns: auto auto auto; text-align:center")
-
-function makeGrid() {
-    for (var i = 0; i < 9; i++) {
-        var box = document.createElement("div");
-        box.setAttribute("style", "width: 100px; height: 100px; border: 1px solid black; background-color: white; text-align:center");
-        box.setAttribute("id", `${i}`);
-        box.className = 'boxes';
-        box.addEventListener("click", handleGridClick);
-        grid.appendChild(box);
+var Presentation = {
+    scoreboard: document.getElementById("scoreboard"),
+    grid: document.getElementById("grid"),
+    makeGrid: function () {
+        for (var i = 0; i < 9; i++) {
+            var box = document.createElement("div");
+            box.setAttribute("style", "width: 100px; height: 100px; border: 1px solid black; background-color: white; text-align:center");
+            box.setAttribute("id", `${i}`);
+            box.className = 'boxes';
+            box.addEventListener("click", this.handleGridClick);
+            this.grid.appendChild(box);
+        
+        }
+        gameState.model = {
+            0: [0,1,2],
+            1: [3,4,5],
+            2: [6,7,8]
+        };
+    },
+    makeNewGame: function (event) {
+        if (event) {
+          event.preventDefault();
+        }
+        for (var i = 0; i < 9; i++) {
+            var box = document.getElementById(i);
+            Presentation.grid.removeChild(box);
+        }
+        Presentation.makeGrid();
+    },
+    renderSymbol: function (id, player) {
+        //place an X everytime player X clicks
+            var symbol = document.createElement("div");
+            symbol.setAttribute("style", "text-align: center; margin-top: 35px;");
+            if (player === player1) {
+                symbol.innerHTML= `<b> X <br>${player}</b>`;
+            } else {
+                symbol.innerHTML= `<b> O <br>${player}</b>`;
+            }
+            id.appendChild(symbol);
+      },
+      changeModel: function (id, player) {
+        if (id < 3) {
+            gameState.model[0][id] = player
+        } else if (id >= 3 && id < 6) {
+            gameState.model[1][id-3] = player
+        } else {
+            gameState.model[2][id-6] = player
+        }
+    },
+    handleGridClick: function (event) {
+        event.preventDefault();
+        var player = findPlayer();
+        var el = document.getElementById(event.srcElement.id);
+        var id = event.srcElement.id;
     
-    }
+        Presentation.renderSymbol(el, player);
+        Presentation.changeModel(id, player);
+        //are rows filled with X or O's?
+        if (checkWin.row(player)) {
+            alert(`${player} won!`);
+            return Presenation.hasWon(++gameState.round,player);
+    
+        };
+        //are columns filled with X or O's?
+        if (checkWin.column(player)) {
+            alert(`${player} won!`);
+            return Presentation.hasWon(++gameState.round,player);
+        };
+        //are diagonals filled with X or O's?
+        if (checkWin.diagonal(player)) {
+            alert(`${player} won!`);
+            return Presentation.hasWon(++gameState.round,player);
+        };
+        //change current player
+        gameState.currentPlayer.X = !gameState.currentPlayer.X;
+        player = findPlayer();
+        playerEl.innerHTML= `Next Up: ${player}`;
+    },
+    hasWon: function (round, winner) {
+        var roundTitle = document.getElementById('round');
+        var winnerTitle = document.getElementById('winner');
+        var roundEl = document.createElement("div");
+        var roundNum = document.createTextNode(`${round}`);
+        roundEl.appendChild(roundNum);
+        roundTitle.appendChild(roundEl);
+      
+        var winnerEl = document.createElement("div");
+        var winnerText = document.createTextNode(`${winner}`);
+        winnerEl.appendChild(winnerText);
+        winnerTitle.appendChild(winnerEl);
+      
+        this.makeNewGame();
+        playerEl.innerHTML= `Next Up: ${winner}`;
+      }
+}
 
-    gameState.model = {
-        0: [0,1,2],
-        1: [3,4,5],
-        2: [6,7,8]
-    };
-};
-
-makeGrid();
+Presentation.makeGrid();
 
 var newGame = document.getElementById('new-game');
-newGame.addEventListener("click", makeNewGame);
+newGame.addEventListener("click", Presentation.makeNewGame);
 
-function makeNewGame (event) {
-    if (event) {
-      event.preventDefault();
-    }
-    for (var i = 0; i < 9; i++) {
-        var box = document.getElementById(i);
-        grid.removeChild(box);
-    }
-    makeGrid();
-}
-
-//create clickHandler everytime one of the 9 boxes are clicked
-
-function handleGridClick(event) {
-    event.preventDefault();
-    var player = findPlayer();
-    var el = document.getElementById(event.srcElement.id);
-    var id = event.srcElement.id;
-
-    renderSymbol(el, player);
-    changeModel(id, player);
-    //are rows filled with X or O's?
-    if (checkWin.row(player)) {
-        alert(`Player ${player} won!`);
-        return hasWon(++gameState.round,player);
-
-    };
-    //are columns filled with X or O's?
-    if (checkWin.column(player)) {
-        alert(`Player ${player} won!`);
-        return hasWon(++gameState.round,player);
-    };
-    //are diagonals filled with X or O's?
-    if (checkWin.diagonal(player)) {
-        alert(`Player ${player} won!`);
-        return hasWon(++gameState.round,player);
-    };
-    //change current player
-    gameState.currentPlayer.X = !gameState.currentPlayer.X;
-    player = findPlayer();
-    playerEl.innerHTML= `Next Up: ${player}`;
-}
-
-function renderSymbol (id, player) {
-    //place an X everytime player X clicks
-        var symbol = document.createElement("div");
-        symbol.setAttribute("style", "text-align: center; margin-top: 35px;")
-        symbol.innerHTML= `<b>${player}</b>`;
-        id.appendChild(symbol);
-  }
-  
-  function changeModel (id, player) {
-      if (id < 3) {
-          gameState.model[0][id] = player
-      } else if (id >= 3 && id < 6) {
-          gameState.model[1][id-3] = player
-      } else {
-          gameState.model[2][id-6] = player
-      }
-  }
 
 var checkWin = {
     row: function (player) {
@@ -160,22 +173,5 @@ var checkWin = {
             return false
         } 
     }
-}
-
-function hasWon (round, winner) {
-  var roundTitle = document.getElementById('round');
-  var winnerTitle = document.getElementById('winner');
-  var roundEl = document.createElement("div");
-  var roundNum = document.createTextNode(`${round}`);
-  roundEl.appendChild(roundNum);
-  roundTitle.appendChild(roundEl);
-
-  var winnerEl = document.createElement("div");
-  var winnerText = document.createTextNode(`${winner}`);
-  winnerEl.appendChild(winnerText);
-  winnerTitle.appendChild(winnerEl);
-
-  makeNewGame();
-  playerEl.innerHTML= `Next Up: ${winner}`;
 }
 };
